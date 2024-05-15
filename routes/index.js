89cc0cf4258d37e5084a195c0f798c0c56579e96
AdiIrawan21 <adiirawan01012003@gmail.com>
2024-05-15 20:27:47 +0700

    feat: updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const restric = require('../middleware/restricted');
-const { register, verifyEmail, requestVerifyEmail } = require('../controllers/auth.controllers');
+const { register, login, whoami, verifyEmail, requestVerifyEmail } = require('../controllers/auth.controllers');
 
 router.post('/register', register);
+router.post('/login', login);
+router.get('/whoami', restric, whoami);
 router.get('/verify', verifyEmail);
 router.get('/request-verify', requestVerifyEmail);
 module.exports = router;
